4be6f9f51253dfadddedff033f09e66a41041c37
Gianni Castellano <gianniccastellano@gmail.com>
2024-05-29 15:36:55 -0400

    displayed items from json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const baseUrl = "http://localhost:3000"
+const availableItems = document.getElementById('shop-items')
 
 document.addEventListener("DOMContentLoaded", function() {
     getItems()
@@ -7,5 +8,18 @@ document.addEventListener("DOMContentLoaded", function() {
 function getItems() {
     fetch(`${baseUrl}/items`)
     .then(response => response.json())
-    .then(items => console.log(items))
+    .then(items => displayItems(items))
 }
+
+function displayItems (items) {
+    items.forEach(item => {
+      displayItem(item)
+    })
+  }
+  
+  function displayItem(item) {
+    const itemImgHolder = document.createElement('img')
+    itemImgHolder.src = item.icon_large
+    availableItems.append(itemImgHolder)
+  }
+
